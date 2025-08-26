a6cfddeeeaac5c0f2f1b6f4cde50e8bbf63a06e4
Sagar Niroula (Alt.) <antihacks36@gmail.com>
2025-08-26 20:34:44 +0545

    First commit - completed final project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.view.LibraryDashboard;
+
+public class Main {
+    public static void main(String[] args) {
+        new LibraryDashboard();
+    }
+}
---
+++ src/com/db/HowGitWorks.java
@@ -0,0 +1,19 @@
+package com.db;
+
+/*
+    Repository, Staging Area, Commit, GitHub
+
+    git init -> turns a normal folder into a Git repository.
+
+    SA -> Waiting room , Files go into staging before they are permanently saved
+            into history.
+
+    Commit -> A snapshot of our project at a given moment.
+            Each commit has a message that describes what has changed.
+
+    GitHub -> A cloud storage for Git repositories.
+            -> Lets you share and collaborate with others.
+ */
+
+public class HowGitWorks {
+}
