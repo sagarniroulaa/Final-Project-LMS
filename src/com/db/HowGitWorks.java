18d8e65f67f413412268a0fe78f0bfbda8cef8a2
Sagar Niroula (Alt.) <antihacks36@gmail.com>
2025-08-26 20:38:45 +0545

    Updated HowGitWorks file

--- src/com/db/HowGitWorks.java
+++ src/com/db/HowGitWorks.java
@@ -10,6 +10,11 @@ package com.db;
 
     Commit -> A snapshot of our project at a given moment.
             Each commit has a message that describes what has changed.
+            ->
+            Please tell me who you are.
+
+            git config --global user.email "you@example.com"
+            git config --global user.name "Your Name"
 
     GitHub -> A cloud storage for Git repositories.
             -> Lets you share and collaborate with others.
